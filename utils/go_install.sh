7f60ecd6822feddefb3e44dab924408b2e7a5730
wychl <wanyanchengli@yeah.net>
2018-02-28 22:05:36 +0800

    install go shell

---
+++ utils/go_install.sh
@@ -0,0 +1,30 @@
+VERSION=1.10
+OS=linux
+ARCH=amd64
+ADDRESS=https://dl.google.com/go
+GOPATH=~/worker/go/
+GOROOT=/usr/local/go
+PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+CONFIG=/etc/profile 
+OLDGOROOT=/usr/local/go 
+INSTALLPATH=/usr/local 
+
+curl -o  go$VERSION.$OS-$ARCH.tar.gz  $ADDRESS/go$VERSION.$OS-$ARCH.tar.gz
+
+
+if [ -e /usr/local/go/ ]; then
+ 
+    rm -r $OLDGOROOT
+
+else
+
+echo export PATH=$PATH >> $CONFIG
+echo export GOROOT=$GOROOT >> $CONFIG
+echo export GOPATH=$GOPATH >> $CONFIG
+
+source $CONFIG
+
+fi 
+
+tar -C $INSTALLPATH -xzf  go$VERSION.$OS-$ARCH.tar.gz
+
